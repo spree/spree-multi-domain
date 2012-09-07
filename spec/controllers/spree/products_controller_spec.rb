a7dea827c59e80ceda5709cbbd75a526464795df
Jonathan Roes <jon@skookum.com>
2012-09-07 17:25:19 -0400

    Update FactoryGirl uses.

--- spec/controllers/spree/products_controller_spec.rb
+++ spec/controllers/spree/products_controller_spec.rb
@@ -3,33 +3,33 @@ require 'spec_helper'
 describe Spree::ProductsController do
 
   before do
-    controller.stub :current_user => Factory(:user)
+    controller.stub :current_user => FactoryGirl.create(:user)
   end
 
   describe 'on :show to a product w/out correct store' do
     before(:each) do
-      @store = Factory(:store)
-      @product = Factory(:product)
+      @store = FactoryGirl.create(:store)
+      @product = FactoryGirl.create(:product)
     end
-    
+
     it 'should return 404' do
       get :show, :id => @product.permalink, :use_route => :spree
-      
+
       response.response_code.should == 404
     end
-    
-    
+
+
   end
-  
+
   describe 'on :show to a product w/ store' do
     before(:each) do
-      @store = Factory(:store)
-      @product = Factory(:product, :stores => [@store])
+      @store = FactoryGirl.create(:store)
+      @product = FactoryGirl.create(:product, :stores => [@store])
     end
-    
+
     it 'should return 200' do
       get :show, :id => @product.permalink, :use_route => :spree
-      
+
       response.response_code.should == 200
     end
   end
