f9e51b9a2037751c9a1b6d24b3a51524b34ab215
Clarke Brunsdon <clarke@freerunningtechnologies.com>
2013-03-08 01:21:44 -0800

    Add support for a logo-file on a store
    Change-Id: I43269b80635a22eaaa94e6cb00a4cd8139f3a4ea

--- app/models/spree/store.rb
+++ app/models/spree/store.rb
@@ -1,5 +1,6 @@
 module Spree
   class Store < ActiveRecord::Base
+    include Spree::Core::S3Support
     has_and_belongs_to_many :products, :join_table => 'spree_products_stores'
     has_many :taxonomies
     has_many :orders
@@ -7,10 +8,17 @@ module Spree
     has_many :payment_methods, :through => :store_payment_methods
 
     validates_presence_of :name, :code, :domains
-    attr_accessible :name, :code, :default, :email, :domains, :payment_method_ids
+    attr_accessible :name, :code, :default, :email, :domains, :payment_method_ids, :logo
 
     scope :default, where(:default => true)
     scope :by_domain, lambda { |domain| where("domains like ?", "%#{domain}%") }
+
+    has_attached_file :logo,
+      :styles => { :mini => '48x48>', :small => '100x100>', :medium => '250x250>' },
+      :default_style => :medium,
+      :url => 'stores/:id/:style/:basename.:extension',
+      :path => 'stores/:id/:style/:basename.:extension',
+      :convert_options => { :all => '-strip -auto-orient' }
 
     def self.current(domain = nil)
       current_store = domain ? Store.by_domain(domain).first : nil
