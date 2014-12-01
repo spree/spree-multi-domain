3a48b35e292673fb29b517c07a075d61521e1d2a
Josua Schmid <josua.schmid@renuo.ch>
2014-12-01 13:56:50 +0100

    add necessary validation for paperclip 4.1

--- app/models/spree/store_decorator.rb
+++ app/models/spree/store_decorator.rb
@@ -19,5 +19,6 @@ module Spree
       :path => 'stores/:id/:style/:basename.:extension',
       :convert_options => { :all => '-strip -auto-orient' }
 
+    validates_attachment_file_name :logo, :matches => [/png\Z/i, /jpe?g\Z/i]
   end
 end
