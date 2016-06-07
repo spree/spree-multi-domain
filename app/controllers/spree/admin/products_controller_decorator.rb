41b06643076cf44e93db6ceb05f965653ec702cb
blset <contact@blset.com>
2016-06-07 15:25:03 +0200

    wrong super call

--- app/controllers/spree/admin/products_controller_decorator.rb
+++ app/controllers/spree/admin/products_controller_decorator.rb
@@ -1,12 +1,13 @@
 Spree::Admin::ProductsController.class_eval do
   update.before :set_stores
+ alias :spree_update :update
 
   def update
     store_ids = params[:product][:store_ids]
     if store_ids.present?
       params[:product][:store_ids] = store_ids.split(',')
     end
-    super
+    spree_update
   end
 
   private
