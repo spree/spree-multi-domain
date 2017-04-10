bfc2e78040b028bed2b967b431114a9948b5a8c3
drakkensaer <shadowdrakkeen@gmail.com>
2017-04-10 19:29:41 +0000

    fixed merged

--- app/models/spree/store_decorator.rb
+++ app/models/spree/store_decorator.rb
@@ -11,36 +11,6 @@ module Spree
     has_many :shipping_methods, through: :store_shipping_methods
 
     has_and_belongs_to_many :promotion_rules, class_name: 'Spree::Promotion::Rules::Store', join_table: 'spree_promotion_rules_stores', association_foreign_key: 'promotion_rule_id'
-<<<<<<< HEAD
-
-    has_attached_file :logo,
-                      styles: { mini: '48x48>', small: '100x100>', logo: '250x250>', large: '600x600>' },
-                      default_style: :logo,
-                      url: '/spree/logos/:id/:style/:basename.:extension',
-                      path: ':rails_root/public/spree/logos/:id/:style/:basename.:extension',
-                      convert_options: { all: '-strip -auto-orient -colorspace sRGB' }
-
-    validate :no_logo_errors
-    validates_attachment :logo,
-      content_type: { content_type: %w(image/jpeg image/jpg image/png image/gif) }
-
-    def find_dimensions
-      temporary = logo.queued_for_write[:original]
-      filename = temporary.path unless temporary.nil?
-      filename = logo.path if filename.blank?
-      geometry = Paperclip::Geometry.from_file(filename)
-      self.logo_width  = geometry.width
-      self.logo_height = geometry.height
-    end
-
-    # if there are errors from the plugin, then add a more meaningful message
-    def no_logo_errors
-      unless logo.errors.empty?
-        # uncomment this to get rid of the less-than-useful interim messages
-        # errors.clear
-        errors.add :logo, "Paperclip returned errors for file '#{logo_file_name}' - check ImageMagick installation or image source file."
-        false
-=======
   
     if ActiveRecord::Base.connection.table_exists?(:spree_stores) && ActiveRecord::Base.connection.column_exists?(:spree_stores, :logo_content_type)
       # save the w,h of the original image (from which others can be calculated)
@@ -75,7 +45,6 @@ module Spree
           errors.add :logo, "Paperclip returned errors for file '#{logo_file_name}' - check ImageMagick installation or image source file."
           false
         end
->>>>>>> master
       end
     end
   end
