4b02d2883f9e6b78b72b298a346769b3601b16aa
Punkbooster <ugliar.arsen@gmail.com>
2019-10-02 10:17:52 +0200

    Update Travis config

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ source 'https://rubygems.org'
 branch = 'master'
 
 gem 'spree',                    github: 'spree/spree',                            branch: branch
-gem 'spree_multi_currency',     github: 'spree/spree_multi_currency',             branch: branch
+gem 'spree_multi_currency',     github: 'Punkbooster/spree_multi_currency',       branch: 'class_eval_to_prepend'
 gem 'spree_analytics_trackers', github: 'spree-contrib/spree_analytics_trackers', branch: branch
 
 gemspec
--- lib/spree/core/controller_helpers/order_decorator.rb
+++ lib/spree/core/controller_helpers/order_decorator.rb
@@ -1,32 +1,19 @@
-Spree::Core::ControllerHelpers::Order.class_eval do
-  def current_currency
-    # When using spree_multi_currency the current_currency is stored within session[:currency]
-    currency = session[:currency] if session.key?(:currency) && supported_currencies.map(&:iso_code).include?(session[:currency])
-    # If there is no session currency fall back to the store default
-    currency ||= current_store.try(:default_currency)
-    # If there is still no currency fall back to Spree default
-    currency = Spree::Config[:currency] if currency.blank?
-    currency
+module Spree
+  module Core
+    module ControllerHelpers
+      module OrderDecorator
+        def current_currency
+          # When using spree_multi_currency the current_currency is stored within session[:currency]
+          currency = session[:currency] if session.key?(:currency) && supported_currencies.map(&:iso_code).include?(session[:currency])
+          # If there is no session currency fall back to the store default
+          currency ||= current_store.try(:default_currency)
+          # If there is still no currency fall back to Spree default
+          currency = Spree::Config[:currency] if currency.blank?
+          currency
+        end
+      end
+    end
   end
 end
 
-
-# module Spree
-#   module Core
-#     module ControllerHelpers
-#       module OrderDecorator
-#         def current_currency
-#           # When using spree_multi_currency the current_currency is stored within session[:currency]
-#           currency = session[:currency] if session.key?(:currency) && supported_currencies.map(&:iso_code).include?(session[:currency])
-#           # If there is no session currency fall back to the store default
-#           currency ||= current_store.try(:default_currency)
-#           # If there is still no currency fall back to Spree default
-#           currency = Spree::Config[:currency] if currency.blank?
-#           currency
-#         end
-#       end
-#     end
-#   end
-# end
-#
-# ::Spree::Core::ControllerHelpers::Order.prepend ::Spree::Core::ControllerHelpers::OrderDecorator
+::Spree::Core::ControllerHelpers::Order.prepend ::Spree::Core::ControllerHelpers::OrderDecorator
